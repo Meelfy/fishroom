98cb1b53b65e36cb5447c95d7c3e59e6a7ef5a7c
georgethrax <georgethrax@gmail.com>
2018-12-01 16:11:36 +0800

    启动脚本

---
+++ start-fishroom.core.sh
@@ -0,0 +1,1 @@
+nohup /home/lix/anaconda3/bin/python -m fishroom.fishroom > nohup.fishroom-core.out &
---
+++ start-fishroom.web.sh
@@ -0,0 +1,2 @@
+echo "fishroom.web uses port 80, sudo needed"
+nohup /home/lix/anaconda3/bin/python -m fishroom.web > nohup.fishroom-web.out &
